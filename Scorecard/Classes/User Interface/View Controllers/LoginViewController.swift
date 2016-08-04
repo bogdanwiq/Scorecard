4bf176f9ffde4afda3a73ee204027e70f048c997
Popa Tiberiu Bogdan <bogdanwiq71@yahoo.com>
2016-08-04 15:37:55 +0300

    Skip login page if user is logged in.

--- Scorecard/Classes/User Interface/View Controllers/LoginViewController.swift
+++ Scorecard/Classes/User Interface/View Controllers/LoginViewController.swift
@@ -19,6 +19,32 @@ class LoginViewController : BaseViewController, GIDSignInUIDelegate, GIDSignInDe
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        if FBSDKAccessToken.currentAccessToken() != nil {
+            var fullName = ""
+            var imageUrl = ""
+            let request1 = FBSDKGraphRequest.init(graphPath: "me/picture", parameters: ["type": "large", "redirect": "false"], HTTPMethod: "GET")
+            request1.startWithCompletionHandler({ (connection, result, error) in
+                if error != nil {
+                    print(error.localizedDescription)
+                    return
+                }
+                imageUrl = result.valueForKey("data")!.valueForKey("url")! as! String
+                let request2 = FBSDKGraphRequest.init(graphPath: "me", parameters: ["fields": "name"], HTTPMethod: "GET")
+                request2.startWithCompletionHandler({ (connection, result, error) in
+                    if error != nil {
+                        print(error.localizedDescription)
+                        return
+                    }
+                    fullName = result.valueForKey("name")! as! String
+                    self.actInd.startAnimating()
+                    self.googleLoginButton.alpha = 0.15
+                    self.facebookLoginButton.alpha = 0.6
+                    self.presentViewController(RootViewController(fullName: fullName, imageUrl: imageUrl), animated: true, completion: nil)
+                })
+            })
+        } else if (GIDSignIn.sharedInstance() != nil) {
+            GIDSignIn.sharedInstance().signInSilently()
+        }
         GIDSignIn.sharedInstance().delegate = self
         GIDSignIn.sharedInstance().uiDelegate = self
     }
@@ -63,7 +89,7 @@ class LoginViewController : BaseViewController, GIDSignInUIDelegate, GIDSignInDe
     func btnFacebookSignInPressed() {
         
         let loginManager = FBSDKLoginManager()
-        
+
         loginManager.logInWithReadPermissions(["public_profile"], fromViewController: self, handler: { (response:FBSDKLoginManagerLoginResult!, error: NSError!) in
             if (error == nil) {
                 if FBSDKAccessToken.currentAccessToken() != nil {
